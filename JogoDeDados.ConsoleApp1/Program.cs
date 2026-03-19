9def01082b67a6f10cb4c4e7d8c462986302583d
Kauan Silva <kauanmacedo338@gmail.com>
2026-03-19 14:10:42 -0300

    first commit

---
+++ JogoDeDados.ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+
+namespace JogoDeDados.ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int limitedechegada = 30;
+        bool JogoemAndamento = true;
+
+        while (JogoemAndamento) //Loop do jogo
+        {
+            int posicaoJogador = 0;
+
+            Console.WriteLine("=============================");
+            Console.WriteLine("Jogo De Dados");
+            Console.WriteLine("=============================");
+
+            Console.Write("Pressione ENTER para lançar um dado");
+            Console.ReadLine();
+
+            int resultado = RandomNumberGenerator.GetInt32(1, 7);
+
+            Console.WriteLine("=============================");
+            Console.WriteLine($"O dado rolou para o número {resultado}");
+            Console.WriteLine("=============================");
+
+            posicaoJogador += resultado;
+
+            if (posicaoJogador < limitedechegada)
+            {
+                Console.Write($"O Jogador está na posição: {posicaoJogador} de {limitedechegada}");
+            }
+        }
+
+
+        while (true) //Loop principal
+        {
+            Console.Write("Deseja continuar? s/N");
+            string? opcaoContinuar = Console.ReadLine()?.ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
+        }
+
+    }
+}
